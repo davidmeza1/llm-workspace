e7b2c793023db16807124db2399f1cf3017696c8
David Meza <david.meza01@gmail.com>
2025-03-16 16:38:19 -0400

    added shiny chat

---
+++ shinychat-main/scripts/update-chat.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# This script downloads new chat CSS/JS assets from py-shiny, and stamps the
+# directory with a GIT_VERSION.
+
+REPO_URL="https://github.com/posit-dev/py-shiny.git"
+BRANCH=main
+DEST_DIR="inst/lib/shiny"
+
+if [ ! -f "shinychat.Rproj" ]; then
+  echo "Error: You must execute this script from the repo root (./scripts/update-chat.sh)."
+  exit 1
+fi
+
+# Clone the repository with sparse-checkout enabled
+git clone -b "$BRANCH" --depth 1 "$REPO_URL" repo_tmp
+
+rm -rf "$DEST_DIR"
+mkdir -p "$DEST_DIR"
+cp -R "repo_tmp/shiny/www/py-shiny/chat" "$DEST_DIR/chat"
+cp -R "repo_tmp/shiny/www/py-shiny/markdown-stream" "$DEST_DIR/markdown-stream"
+cp -R "repo_tmp/shiny/www/py-shiny/text-area" "$DEST_DIR/text-area"
+(cd repo_tmp; git rev-parse HEAD) > "${DEST_DIR}/GIT_VERSION"
+rm -rf repo_tmp
